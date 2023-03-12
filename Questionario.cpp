4191d52498259d5436327758906628b92723f441
Paulo Sergio <pscosta2008@gmail.com>
2023-03-12 15:10:15 -0300

    Questoes da dificuldade 'fácil' e 'média' finalizada... dificuldade 'dificil' nao está pronta

--- Questionario.cpp
+++ Questionario.cpp
@@ -21,7 +21,7 @@ int q1(string diff)
 	cout << "Dada a equacao x = 2 + 6 . 6, qual o valor de x?\nResposta:";
 	cin >> resQ1;
 	if(resQ1 == 38){
-		cout << "Voce acertou! Proxima questao...\n";
+		cout << "Voce acertou! Proxima questao...\n\n";
 		Sleep (800);
 		return 1;
 	}
@@ -29,15 +29,15 @@ int q1(string diff)
 		cout << "Voce errou, tente novamente: ";
 		cin >> resQ1;
 		if(resQ1 == 38){
-			cout << "Voce acertou! Proxima questao...\n";
+			cout << "Voce acertou! Proxima questao...\n\n";
 			Sleep (800);
 			return 1;
 		}
-		cout << "Voce errou. Proxima questao";
+		cout << "Voce errou. Proxima questao...\n\n";
 		return 0;
 	}
 	else if(resQ1 != 38 && diff == medium){
-		cout << "Voce errou. Proxima questao";
+		cout << "Voce errou. Proxima questao...\n\n";
 		return 0;
 	}
 	else if(resQ1 != 38 && diff == hard){
@@ -49,8 +49,9 @@ int q2(string diff)
 	string easy = "easy", medium = "medium", hard = "hard";
 	int resQ2;
 	cout << "Resolva: 4x = 44: ";
+	cin >> resQ2;
 	if(resQ2 == 11){
-		cout << "Voce acertou! Proxima questao...\n";
+		cout << "Voce acertou! Proxima questao...\n\n";
 		Sleep (800);
 		return 1;
 	}
@@ -58,29 +59,29 @@ int q2(string diff)
 		cout << "Voce errou, tente novamente: ";
 		cin >> resQ2;
 		if(resQ2 == 11){
-			cout << "Voce acertou! Proxima questao...\n";
+			cout << "Voce acertou! Proxima questao...\n\n";
 			Sleep (800);
 			return 1;
 		}
-		cout << "Voce errou. Proxima questao";
+		cout << "Voce errou. Proxima questao...\n\n";
 		return 0;
 	}
 	else if(resQ2 != 11 && diff == medium){
-		cout << "Voce errou. Proxima questao";
+		cout << "Voce errou. Proxima questao...\n\n";
 		return 0;
 	}
 	else if(resQ2 != 11 && diff == hard){
 		return 0;	
 	}
 }
-void q3(int resQ2)
+int q3(string diff)
 {
 	string easy = "easy", medium = "medium", hard = "hard";
 	int resQ3;
-	cout << "Joao quer dividir 25 laranajas com 5 amigos. Quantas laranjas cada amigo recebera, sabendo que Joao nao ficara com nenhuma fruta?\nResposta";
+	cout << "Joao quer dividir 25 laranajas com 5 amigos. Quantas laranjas cada amigo recebera, sabendo que Joao nao ficara com nenhuma fruta?\nResposta: ";
 	cin >> resQ3;
 	if(resQ3 == 5){
-		cout << "Voce acertou! Proxima questao...\n";
+		cout << "Voce acertou! Proxima questao...\n\n";
 		Sleep (800);
 		return 1;
 	}
@@ -88,42 +89,81 @@ void q3(int resQ2)
 		cout << "Voce errou, tente novamente: ";
 		cin >> resQ3;
 		if(resQ3 == 5){
-			cout << "Voce acertou! Proxima questao...\n";
+			cout << "Voce acertou! Proxima questao...\n\n";
 			Sleep (800);
 			return 1;
 		}
-		cout << "Voce errou. Proxima questao";
+		cout << "Voce errou. Proxima questao\n\n";
 		return 0;
 	}
 	else if(resQ3 != 5 && diff == medium){
-		cout << "Voce errou. Proxima questao";
+		cout << "Voce errou. Proxima questao\n\n";
 		return 0;
 	}
 	else if(resQ3 != 5 && diff == hard){
 		return 0;	
 	}
 	
-	}
 }
-void q4(int resQ3)
+int q4(string diff)
 {
-	if (resQ3 == 5)
-	{
-		cout << "Voce acertou! Proxima questao...\n";
-		Sleep(800);
-		cout << "Sabendo que um quadrado tem seus lados medindo 2 metros, qual eh a area do mesmo?";
+	string easy = "easy", medium = "medium", hard = "hard";
+	int resQ4;
+	cout << "Sabendo que um quadrado tem seus lados medindo 2 metros, qual eh a area do mesmo?";
+	cin >> resQ4;
+	if(resQ4 == 4){
+		cout << "Voce acertou! Proxima questao...\n\n";
+		Sleep (800);
+		return 1;
+	}
+	else if(resQ4 != 4 && diff == easy){
+		cout << "Voce errou, tente novamente: ";
+		cin >> resQ4;
+		if(resQ4 == 4){
+			cout << "Voce acertou! Proxima questao...\n\n";
+			Sleep (800);
+			return 1;
+		}
+		cout << "Voce errou. Proxima questao\n\n";
+		return 0;
+	}
+	else if(resQ4 != 4 && diff == medium){
+		cout << "Voce errou. Proxima questao\n\n";
+		return 0;
+	}
+	else if(resQ4 != 4 && diff == hard){
+		return 0;	
 	}
 }
-void q5(int resQ4)
-{
-	if (resQ4 == 4)
-	{
-		cout << "Voce acertou! Ultima questao...\n";
-		Sleep(800);
-		cout << "Pedro precisa juntar 4000 reais para comprar um computador novo. Em quantos meses ele consiguira esse dinheiro ganhando uma mesada de 200 reais?\nResposta:";
+int q5(string diff){
+	string easy = "easy", medium = "medium", hard = "hard";
+	int resQ5;
+	cout << "Pedro precisa juntar 4000 reais para comprar um computador novo. Em quantos meses ele consiguira esse dinheiro ganhando uma mesada de 200 reais?\nResposta:";
+	cin >> resQ5;
+	if(resQ5 == 20){
+		cout << "Voce acertou!\n\n";
+		Sleep (800);
+		return 1;
+	}
+	else if(resQ5 != 20 && diff == easy){
+		cout << "Voce errou, tente novamente: ";
+		cin >> resQ5;
+		if(resQ5 == 20){
+			cout << "Voce acertou!\n\n";
+			Sleep (800);
+			return 1;
+		}
+		cout << "Voce errou.\n\n";
+		return 0;
+	}
+	else if(resQ5 != 20 && diff == medium){
+		cout << "Voce errou.\n\n";
+		return 0;
+	}
+	else if(resQ5 != 20 && diff == hard){
+		return 0;	
 	}
 }
-
 boolean instructionDificult(string diff, string name){
 	system("CLS");
 	string easy = "easy", medium = "medium", hard = "hard";
@@ -154,7 +194,6 @@ int main()
 	cout << "Para continuar a prova, confirme seus dados: " << name << ", " << age << " anos\nDigite 'confirmar' se deseja continuar a prova com esses dados\nDigite 'voltar' caso deseja reescrever seus dados\n";
 	cin >> confirm;
 	do{
-
 		if (confirm == back){
 			system("CLS");
 			nameM();
@@ -202,69 +241,10 @@ int main()
 			int results = 0;
 			results += q1(diff); 
 			results += q2(diff);
-			cout << "total de seus pontos sao " << results;
-				/*
-				cin >> resQ1;
-				if (resQ1 != 38)
-				{
-					cout << "Resposta errada, voce tem mais uma chance:\n";
-					cin >> resQ1;
-					if (resQ1 != 38)
-					{
-						cout << "Resposta errada novamente... Proxima questao:\n";
-						q2(resQ1);
-						cin >> resQ2;
-						if (resQ2 != 11)
-						{
-							cout << "Resposta errada, voce tem mais uma chance: ";
-							cin >> resQ2;
-							if (resQ2 != 11)
-							{
-								cout << "Resposta errada novamente... Proxima questao:\n";
-								q3(resQ2);
-							}
-						}
-					}
-				}
-
-				q3(resQ2);
-				cin >> resQ3;
-				if (resQ3 != 5)
-				{
-					cout << "Resposta errada, voce tem mais uma chance: ";
-					cin >> resQ3;
-					if (resQ3 != 5)
-					{
-						cout << "Resposta errada novamente... Proxima questao:\n";
-						q4(resQ3);
-					}
-				}
-				q4(resQ3);
-				cin >> resQ4;
-				if (resQ4 != 4)
-				{
-					cout << "Resposta errada, voce tem mais uma chance:";
-					cin >> resQ3;
-					if (resQ4 != 4)
-					{
-						cout << "Resposta errada novamente... Ultima questao:\n";
-						q5(resQ4);
-					}
-				}
-			}
-			/*
-			if (diff == medium)
-			{
-				system("CLS");
-				cout << "Voce escolheu a dificuldade 'media'.\nINSTRUCOES: Ola " << name << " , nesta dificuldade, voce tem apenas uma chance para acertar a questao. Boa sorte!";
-			}
-
-			if (diff == hard)
-			{
-				system("CLS");
-				cout << "Voce escolheu a dificuldade 'dificil'.\nINSTRUCOES: Ola " << name << " , nesta dificuldade, alem de ter apenas uma chance para acertar a questao, caso erre, reiniciara a prova inteira. Boa sorte!";
-			}
-			*/		
+			results += q3(diff);
+			results += q4(diff);
+			results += q5(diff);
+			cout << "Parabens, voce terminou a prova! Voce tirou:" << results << "/5";
 		}
 	} while (action != confirm);
 
